b83df5cd386532b515e6f5ec6a06e0eb614a3329
George Alexandru Pintea <george.pintea@stud.ubbcluj.ro>
2024-07-03 18:59:57 +0300

    Create SortedBagIterator.h

---
+++ Semester 2/DSA/Assignment3SortedBagDLLA/SortedBagIterator.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "SortedBag.h"
+
+class SortedBag;
+
+class SortedBagIterator
+{
+	friend class SortedBag;
+
+private:
+	const SortedBag& bag;
+	SortedBagIterator(const SortedBag& b);
+
+	int current;
+	int freq;
+	bool forward;
+
+public:
+	TComp getCurrent();
+	bool valid() const;
+	void next();
+	void first();
+};
